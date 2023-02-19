0c64381e2a3d20141840a82eba59428e8902ea03
blakspex <107747799+blakspex@users.noreply.github.com>
2023-02-20 01:15:36 +0530

    Update Test Runner
    features updated from local to source

--- src/test/java/testRunner/myTestRunner.java
+++ src/test/java/testRunner/myTestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = {"C:/THE OTHER DRIVE/worrkbench/CucumberPom/src/test/resources/features"},
+		features = {"src/test/resources/features"},
 		glue = {"stepDefinitions","AppHooks"},
 		plugin = {"pretty", "html:target/report2"},
 		monochrome = true
